13a1e594c81f58935f060874b556f99ad204fdaa
Matt Eva <matteweva@gmail.com>
2025-10-12 07:09:53 -0700

    set up quill editor

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,7 +2,6 @@ import { API_URL } from '$env/static/private';
 
 export async function load() {
 	const apiBase = API_URL;
-
 	return {
 		props: {
 			apiBase
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<slot />
+<script>
+	let { children } = $props();
+</script>
+
+<header>
+	<nav>
+		<a href="/">home</a>
+		<a href="/write">write</a>
+	</nav>
+</header>
+{@render children()}
