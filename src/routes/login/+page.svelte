bb7f2a5125b5e73ce55fc51b18da3011959f51c1
Matt Eva <matteweva@gmail.com>
2025-11-05 20:31:33 -0800

    implemented delete account

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { goto } from '$app/navigation';
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 	import birthdayChecker from '$lib/birthdayChecker.js';
 	let { data } = $props();
 	const apiBase = data.props.apiBase;
@@ -15,6 +15,12 @@
 	let birthdayErrorMessages = $state(startingBirthdayErrorMessages);
 
 	let user = getContext('user');
+
+	onMount(() => {
+		if (user.loggedIn === true) {
+			goto('/profile');
+		}
+	});
 
 	async function login(e) {
 		e.preventDefault();
@@ -55,6 +61,8 @@
 					birthYear: parseInt(birthYear)
 				})
 			});
+			user.loggedIn = true;
+			goto('/profile');
 		} catch (error) {
 			console.error(error);
 		}
@@ -62,33 +70,35 @@
 </script>
 
 <div>
-	<form onsubmit={login}>
-		<label for="email">Login with an email One Time Password</label>
-		<input type="text" name="email" placeholder="email@example.com" />
-		<input type="submit" value="send code" />
-	</form>
-	<form onsubmit={signup}>
-		<label for="email">Sign Up with email</label>
-		<input type="text" name="email" placeholder="email@example.com" bind:value={signupEmail} />
+	{#if user.loggedIn !== true}
+		<form onsubmit={login}>
+			<label for="email">Login with an email One Time Password</label>
+			<input type="text" name="email" placeholder="email@example.com" />
+			<input type="submit" value="send code" />
+		</form>
+		<form onsubmit={signup}>
+			<label for="email">Sign Up with email</label>
+			<input type="text" name="email" placeholder="email@example.com" bind:value={signupEmail} />
 
-		<label for="birthday">Please enter your birthday</label>
-		<section name="birthday">
-			<label for="month">Month</label>
-			<input type="text" name="month" placeholder="month" bind:value={birthMonth} />
-			{#if birthdayErrorMessages.month}
-				<p>{birthdayErrorMessages.month}</p>
-			{/if}
-			<label for="day">Day</label>
-			<input type="text" name="day" placeholder="day" bind:value={birthDay} />
-			{#if birthdayErrorMessages.day}
-				<p>{birthdayErrorMessages.day}</p>
-			{/if}
-			<label for="year">Year</label>
-			<input type="text" name="year" placeholder="year" bind:value={birthYear} />
-			{#if birthdayErrorMessages.year}
-				<p>{birthdayErrorMessages.year}</p>
-			{/if}
-		</section>
-		<input type="submit" value="sign up" />
-	</form>
+			<label for="birthday">Please enter your birthday</label>
+			<section name="birthday">
+				<label for="month">Month</label>
+				<input type="text" name="month" placeholder="month" bind:value={birthMonth} />
+				{#if birthdayErrorMessages.month}
+					<p>{birthdayErrorMessages.month}</p>
+				{/if}
+				<label for="day">Day</label>
+				<input type="text" name="day" placeholder="day" bind:value={birthDay} />
+				{#if birthdayErrorMessages.day}
+					<p>{birthdayErrorMessages.day}</p>
+				{/if}
+				<label for="year">Year</label>
+				<input type="text" name="year" placeholder="year" bind:value={birthYear} />
+				{#if birthdayErrorMessages.year}
+					<p>{birthdayErrorMessages.year}</p>
+				{/if}
+			</section>
+			<input type="submit" value="sign up" />
+		</form>
+	{/if}
 </div>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,6 +4,7 @@
 	const { data } = $props();
 	const apiBase = data.props.apiBase;
 	let user = getContext('user');
+	let deletePopup;
 
 	onMount(() => {
 		if (!user.loggedIn) {
@@ -12,15 +13,47 @@
 	});
 
 	async function logout() {
-		await fetch(apiBase + '/logout', {
-			credentials: 'include',
-			method: 'Delete'
-		});
-		user.loggedIn = false;
-		goto('/');
+		try {
+			await fetch(apiBase + '/logout', {
+				credentials: 'include',
+				method: 'DELETE'
+			});
+			user.loggedIn = false;
+			goto('/');
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function deleteAccount() {
+		try {
+			await fetch(apiBase + '/delete-account', {
+				credentials: 'include',
+				method: 'DELETE'
+			});
+			user.loggedIn = false;
+			goto('/');
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	function openDeletePopup() {
+		deletePopup.showModal();
+	}
+
+	function closeDeletePopup() {
+		deletePopup.close();
 	}
 </script>
 
 <div>
 	<button onclick={logout}>logout</button>
+	<button onclick={openDeletePopup}>Delete Account</button>
+	<dialog bind:this={deletePopup}>
+		<!-- add in email code delete functionality -->
+		<h2>Are you sure you want to delete your account?</h2>
+		<button onclick={deleteAccount}>yes</button>
+		<button onclick={closeDeletePopup}>no</button>
+	</dialog>
 </div>
