aa332b94a7a2e58658a2f885f0add6f9c416ae5c
Matt Eva <matteweva@gmail.com>
2025-11-12 20:12:38 -0800

    started updating topics in topic boxx

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -11,8 +11,8 @@
 		<label for="writing-type">Writing Type</label>
 		<select name="writing-type" bind:value={writingType}>
 			<option value="All Writing">All Writing</option>
-			<option value="Essays & Blogs">Essays & Blogs</option>
 			<option value="Poetry">Poetry</option>
+			<option value="Essays & Blogs">Essays & Blogs</option>
 			<option value="All Fiction">Fiction</option>
 			<option value="Short Stories">Short Stories</option>
 			<option value="Novelettes">Novellettes</option>
@@ -40,34 +40,36 @@
 				<label for="art">Art</label>
 				<input type="checkbox" name="comedy" />
 				<label for="comedy">Comedy</label>
-				<input type="checkbox" name="comedy" />
-				<label for="comedy">Comedy</label>
 				<input type="checkbox" name="drama" />
-				<label for="drama">Drama</label>
+				<label for="drama">Culture</label>
 				<input type="checkbox" name="fantasy" />
-				<label for="fantasy">Fantasy</label>
+				<label for="fantasy">Fiction</label>
 				<input type="checkbox" name="historical" />
 				<label for="historical">Historical Fiction</label>
 				<input type="checkbox" name="horror" />
 				<label for="horror">Horror</label>
 				<input type="checkbox" name="literary" />
 				<label for="literary">Literary Fiction</label>
-				<input type="checkbox" name="Magical Realism" />
-				<label for="Magical Realism">Magical Realism</label>
+				<input type="checkbox" name="Music" />
+				<label for="Music">Music</label>
 				<input type="checkbox" name="realism" />
-				<label for="realism">Realism</label>
-				<input type="checkbox" name="romance" />
-				<label for="romance">Romance</label>
-				<input type="checkbox" name="science" />
-				<label for="science">Science Fiction</label>
+				<label for="realism">Film and TV</label>
+				<input type="checkbox" name="politics" />
+				<label for="politics">Politics</label>
+				<input type="checkbox" name="politics" />
+				<label for="psychology">Psychology</label>
+				<input type="checkbox" name="psychology" />
+				<label for="philosophy">Philosophy</label>
+				<input type="checkbox" name="philosophy" />
+				<label for="science">Science</label>
 				<input type="checkbox" name="social" />
-				<label for="social">Social Fiction</label>
+				<label for="social">Social Dynamics</label>
 				<input type="checkbox" name="speculative" />
 				<label for="speculative">Speculative Fiction</label>
-				<input type="checkbox" name="supernatural" />
-				<label for="supernatural">Supernatural</label>
 				<input type="checkbox" name="thriller" />
-				<label for="thriller">Thriller</label>
+				<label for="thriller">Technology</label>
+				<input type="checkbox" name="supernatural" />
+				<label for="supernatural">Video Games</label>
 			</div>
 		{/if}
 		{#if writingType === 'All Fiction' || writingType === 'Short Stories' || writingType === 'Novellettes' || writingType === 'Novellas' || writingType === 'Novels'}
