7d0674b55ac98286bfa99ab573befbb3339b9fde
Matt Eva <matteweva@gmail.com>
2025-11-28 09:34:22 -0800

    verified page.js files run on server

--- src/routes/profile/+page.js
+++ src/routes/profile/+page.js
@@ -2,7 +2,9 @@ export const load = async ({ fetch, parent }) => {
 	const data = await parent();
 	const apiBase = data.props.apiBase;
 	try {
-		const res = await fetch(apiBase + '/user-creators');
+		const time = Date.now();
+		console.log(time);
+		const res = await fetch(apiBase + '/user-creators', { credentials: 'include' });
 		if (res.ok) {
 			const data = await res.json();
 			console.log(data);
